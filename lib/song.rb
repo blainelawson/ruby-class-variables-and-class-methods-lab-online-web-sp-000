d54f1ece52218981f8cd75c6401b44ebe75b6339
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-12 20:01:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,29 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artists, :genres
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+
+    if !@@artist.include?(artist)
+      @@artists << artist
+    end
+
+    if !@@genres.include?(genre)
+      @@genres << genre
+    end
+
   end
+
+  def self.count
+    @@count
+  end
+
 
 
 end
